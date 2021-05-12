900fff3562c83d19330403506e1c5c06995c92da
Christian Bjørnsrud <45819619+ckb78@users.noreply.github.com>
2021-05-12 15:47:11 +0200

    Update Satellite.java
    Added simple description of how to add satelites.

--- src/main/java/net/ckb78/sattracker/controllers/Satellite.java
+++ src/main/java/net/ckb78/sattracker/controllers/Satellite.java
@@ -1,5 +1,11 @@
 package net.ckb78.sattracker.controllers;
 
+/**
+* The Satellite class allows to add as many satelites as wanted.
+* Add new satelite as a valued enum below ISS. 
+* Then create a DTO that corresponds to the JSON from the API. 
+*/
+
 public enum Satellite {
     ISS("http://api.open-notify.org/iss-now.json");
 
